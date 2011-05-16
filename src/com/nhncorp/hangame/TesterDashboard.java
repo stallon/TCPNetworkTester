2d7eb2908cd0ccf3f026cbb2101673b07a20a0d0
Hong SeongBong <stallon72@naver.com>
2011-05-16 15:10:35 +0900

    test commit

--- src/com/nhncorp/hangame/TesterDashboard.java
+++ src/com/nhncorp/hangame/TesterDashboard.java
@@ -9,5 +9,7 @@ public class TesterDashboard extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        System.out.println("adsfsadf");
     }
 }
